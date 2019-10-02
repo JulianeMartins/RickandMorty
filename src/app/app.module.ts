f83fa088527fee86031a4a23455285aa22d14d17
Juliane Martins de Vargas <martinsjuliane@icloud.com>
2019-10-02 15:06:44 -0300

    layouts, fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LocationComponent } from './components/location/location.component';
 import { CharacterComponent } from './components/character/character.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import { CharacterComponent } from './components/character/character.component';
     EpisodeComponent,
     LocationComponent,
     CharacterComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
